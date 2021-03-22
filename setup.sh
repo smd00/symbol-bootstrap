53eff2c1a9018cd13b08677436c6913e38d20652
Daniel Montoya <daniel@montoya.com.au>
2021-03-22 14:15:26 +1100

    Added sudo

--- setup.sh
+++ setup.sh
@@ -6,13 +6,12 @@
 
 # Usage:
 # git clone https://github.com/smd00/symbol-bootstrap.git && mv -f symbol-bootstrap ${HOME}/symbol && cd ${HOME}/symbol && chmod +x ./setup.sh
-# customise .env.tmp and cron.tmp
 # sh ./setup.sh
 
 # =============================================
 # Update system and install dependencies
 
-apt update
-apt install nodejs npm \ docker docker-compose
-npm install -g symbol-bootstrap
-symbol-bootstrap start -p bootstrap
+sudo apt update
+sudo apt install nodejs npm \ docker docker-compose
+sudo npm install -g symbol-bootstrap
+sudo symbol-bootstrap start -p bootstrap
